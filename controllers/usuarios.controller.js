63444db2866d60b99a609ecd66964c4ba868c805
caAlvaradoh69 <carlos.alvarado.hernandez95@gmail.com>
2021-04-09 13:39:19 -0400

    primer commit

---
+++ controllers/usuarios.controller.js
@@ -0,0 +1,75 @@
+const {response, request} =  require ('express');
+
+
+
+
+const usuariosGet = (req = request, res = response) => {
+    
+    const {q, nombre = 'no name', apikey, page = 1, limit } = req.query; 
+    
+    res.json({
+        ok: true,
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    
+    const { id } = req.params;    
+    
+    res.json({
+        ok: true,
+        msg: 'put API - controlador',
+        id
+
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    
+    const {nombre, edad } = req.body;
+
+    
+    res.json({
+        ok: true,
+        msg: 'post API - controlador',
+        nombre, 
+        edad 
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'patch API - controlador'
+
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'delete API - controlador'
+
+    });
+}
+
+
+
+
+
+module.exports = { 
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosPatch,
+    usuariosDelete
+    
+
+}
---
+++ routes/usuarios.js
@@ -0,0 +1,27 @@
+const {Router} = require('express');
+const { usuariosGet, 
+        usuariosPut, 
+        usuariosPost, 
+        usuariosDelete, 
+        usuariosPatch } = require('../controllers/usuarios.controller');
+
+
+
+
+const router = Router();
+
+router.get('/', usuariosGet );
+
+router.put('/:id', usuariosPut);
+
+router.post('/', usuariosPost );
+
+router.patch('/', usuariosPatch);
+
+router.delete('/', usuariosDelete);
+
+
+
+
+
+module.exports = router;
